b056f8179807b6c0b41cccb8c858115942979243
Kazuki Asai <PrincessDaisy@KazukiAsai-no-MacBook-Air.local>
2015-09-29 19:56:11 +0900

    Initial Commit

---
+++ Object_Test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Object_Test
+//
+//  Created by Kazuki Asai on 2015/09/29.
+//  Copyright (c) 2015年 Kazuki Asai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
